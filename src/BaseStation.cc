fa5c7e43bbf7ae011b360e508556206336fd90a1
Users <Users@10.102.19.243>
2020-01-22 15:30:36 +0100

    Add functions and parameters for BaseStation

--- src/BaseStation.cc
+++ src/BaseStation.cc
@@ -3,11 +3,123 @@
 
 Define_Module(BaseStation);
 
-void BaseStation::initialize() {
+BaseStation::Basestation()
+{
+    packetServiced = nullptr;
+    endServiceMsg = nullptr;
 }
 
-void BaseStation::handleMessage(cMessage *msg) {
+BaseStation::~BaseStation()
+{
+    delete packetServiced;
+    cancelAndDelete(endServiceMsg);
 }
 
-void BaseStation::finish() {
+void BaseStation::initialize()
+{
+    // Statistics (signals)
+    droppedSignal = registerSignal("dropped");
+    queueingTimeSignal = registerSignal("queueingTime");
+    queueLengthSignal = registerSignal("queueLength");
+    emit(queueLengthSignal, 0);
+    busySignal = registerSignal("busy");
+    emit(busySignal, false);
+
+    // Parameters of the queue of BS
+    endServiceMsg = new cMessage("end-service");
+    fifo = par("fifo");
+    capacity = par("capacity");
+    queue.setName("queue");
+}
+
+void BaseStation::handleMessage(cMessage *msg)
+{
+    // When the server finishes to process a packet
+    if (msg == endServiceMsg) {
+        endService(packetServiced);
+        // when the queue is empty : we don't have another packet to process
+        if (queue.isEmpty()) {
+            packetServiced = nullptr;
+            emit(busySignal, false); // the queue is not busy anymore
+        }
+        // when the queue is not empty (at least one packet)
+        else {
+            // we get the packet from the queue to process it
+            packetServiced = getFromQueue();
+            emit(queueLengthSignal, length());
+            // we start counting the service time for the packet
+            simtime_t serviceTime = startService(packetServiced);
+            // we schedule a endServiceMsg after the service time
+            scheduleAt(simTime()+serviceTime, endServiceMsg);
+        }
+    }
+    else {
+        //a packet arrives to the BS from an Aircraft
+        Packet *packet = check_and_cast<Packet *>(msg);
+        arrival(packet);
+
+        if (!packetServiced) {
+            // processor was idle
+            packetServiced = packet;
+            emit(busySignal, true);
+            // we start counting the service time for the packet
+            simtime_t serviceTime = startService(packetServiced);
+            // we schedule a endServiceMsg after the service time
+            scheduleAt(simTime()+serviceTime, endServiceMsg);
+        }
+        else {
+            // we insert the packet in the queue since it cannot be processed for now
+            queue.insert(packet);
+            emit(queueLengthSignal, length());
+            packet->setQueueCount(packet->getQueueCount() + 1);
+        }
+    }
+}
+
+void BaseStation::refreshDisplay() const
+{
+    getDisplayString().setTagArg("i2", 0, packetServiced ? "status/execute" : "");
+    getDisplayString().setTagArg("i", 1, queue.isEmpty() ? "" : "green");
+}
+
+Packet *BaseStation::getFromQueue()
+{
+    Packet *packet;
+    packet = (Packet *)queue.pop();
+    return packet;
+}
+
+int BaseStation::length()
+{
+    return queue.getLength();
+}
+
+void BaseStation::arrival(Packet *packet)
+{
+    packet->setTimestamp();
+}
+
+simtime_t BaseStation::startService(Packet *packet)
+{
+    // gather queueing time statistics
+    simtime_t d = simTime() - packet->getTimestamp();
+    emit(queueingTimeSignal, d);
+    packet->setTotalQueueingTime(packet->getTotalQueueingTime() + d);
+    EV << "Starting service of " << packet->getName() << endl;
+    packet->setTimestamp();
+    // s = T.d², T = 0.03
+    par("serviceTime").setDoubleValue(0.03*pow(packet->par("d").doubleValue(),2));
+    return par("serviceTime").doubleValue();
+}
+
+void BaseStation::endService(packet *packet)
+{
+    EV << "Finishing service of " << packet->getName() << endl;
+    simtime_t d = simTime() - packet->getTimestamp();
+    packet->setTotalServiceTime(packet->getTotalServiceTime() + d);
+    send(packet, "out");
+}
+
+void BaseStation::finish()
+{
 }
--- src/BaseStation.h
+++ src/BaseStation.h
@@ -11,10 +11,38 @@ using namespace omnetpp;
  */
 class BaseStation : public cSimpleModule {
     private:
+        // signals
+        simsignal_t droppedSignal;
+        simsignal_t queueLengthSignal;
+        simsignal_t queueingTimeSignal;
+        simsignal_t busySignal;
+
+        Packet *packetServiced;
+        cMessage *endServiceMsg;
+        cQueue queue;
+
+        // characteristics
+        int capacity;
+        bool fifo;
+
+        //
+        Packet *getFromQueue();
+
+    public:
+        BaseStation();
+        virtual ~BaseStation();
+        int length();
+
     protected:
-        virtual void initialize();
-        virtual void handleMessage(cMessage *msg);
-        virtual void finish();
+        virtual void initialize() override;
+        virtual void handleMessage(cMessage *msg) override;
+        virtual void refreshDisplay() const override;
+        virtual void finish() override;
+
+        // behavior
+        virtual void arrival(Packet *packet);
+        virtual simtime_t startService(Packet *packet);
+        virtual void endService(Packet *packet);
 };
 
 #endif /* BASESTATION_H_ */
